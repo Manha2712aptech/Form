51c494730790619a0bed64303db43e4dc04da3e6
asp <asp@Aptechnk1.edu>
2026-02-03 20:46:32 +0500

    adding code

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-"" 
+const form = document.getElementById("registrationForm");
+const confirmModal = document.getElementById("confirmModal");
+const successModal = document.getElementById("successModal");
+
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const okBtn = document.getElementById("okBtn");
+
+confirmModal.style.display = "none";
+successModal.style.display = "none";
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return;
+  }
+
+  confirmModal.style.display = "flex";
+});
+
+yesBtn.addEventListener("click", function () {
+  confirmModal.style.display = "none";
+  successModal.style.display = "flex";
+  form.reset();
+});
+
+noBtn.addEventListener("click", function () {
+  confirmModal.style.display = "none";
+  form.reset();
+});
+
+okBtn.addEventListener("click", function () {
+  successModal.style.display = "none";
+});
